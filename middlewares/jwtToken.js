169e4c2569bacdd45bf5400dadb4026a588b7b29
prynsh7 <priyanshraj45@gmail.com>
2024-01-06 22:16:49 +0530

    jwt code :: refactor

--- middlewares/jwtToken.js
+++ middlewares/jwtToken.js
@@ -7,36 +7,44 @@ import User from "../models/User.js"
 export const jwtToken = async (req, res, next) => {
     const token = req?.headers?.authorization?.split(" ")[1] || ""
 
-    console.log(token)
+
 
     if (token) {
 
 
-        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        try {
+
+            const decoded = await jwt.verify(token, process.env.JWT_SECRET);
 
 
+            if (!decoded) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Invalid token"
+                })
+            }
 
-        if (!decoded) {
+            const userId = decoded.id;
+
+            const user = await User.findById(userId);
+            if (!user) {
+                return res.status(401).json({
+                    success: false,
+                    message: "No user found"
+                })
+            }
+            req.user = user;
+            return next()
+
+        } catch (err) {
             return res.status(401).json({
                 success: false,
                 message: "Invalid token"
             })
-        }
 
-        const userId = decoded.id;
-
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(401).json({
-                success: false,
-                message: "No user found"
-            })
         }
-        req.user = user;
-       return next()
 
     }
-
     return res.status(401).json({
         success: false,
         message: "No token found"
